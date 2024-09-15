34c36ea9e26cae667630b5ec00d2205e0225083b
Tkr-karan <karankumar072508@gmail.com>
2024-09-15 13:41:48 +0530

    Initial commit with basic API endpoints

---
+++ src/main/java/com/example/journalApp/Controller/JournalEntityController.java
@@ -0,0 +1,44 @@
+package com.example.journalApp.Controller;
+
+
+import com.example.journalApp.Entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntityController {
+
+    public Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public String createJournalEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return "Your journal is successfully Created";
+    }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove((myId));
+    }
+
+    @PutMapping("/id/{myId}")
+    public String updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        journalEntries.put(myId, myEntry);
+        return "your entry is successfully Updated";
+    }
+}
