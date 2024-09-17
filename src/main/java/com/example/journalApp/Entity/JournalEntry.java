fe1d46ede1388c75a6f74aac2d288365712442b3
Tkr-karan <karankumar072508@gmail.com>
2024-09-17 21:24:43 +0530

    add project lambok dependency for make our code clean

--- src/main/java/com/example/journalApp/Entity/JournalEntry.java
+++ src/main/java/com/example/journalApp/Entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.example.journalApp.Entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+// here instead of tellin one-by-one lambok function we can use data annotation from lambok that contains
+// all methods inside it.
+//@Getter
+//@Setter
+@Data
 public class JournalEntry {
 
     @Id
@@ -15,36 +23,4 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
